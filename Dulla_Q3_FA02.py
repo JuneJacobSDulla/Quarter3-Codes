03b92ffb026fb7352a817b91097c6f9d22536b69
JuneJacobSDulla <junejacob.dulla@smc.pshs.edu.ph>
2026-01-14 20:05:24 +0800

    Add files via upload

---
+++ Dulla_Q3_FA02.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+steps = np.array([
+    [4500, 5200, 4800, 5000, 5300],  # Me
+    [4000, 4100, 3900, 4200, 4600],  # Lia
+    [6000, 5800, 5900, 6100, 6200]   # Jake
+])
+
+print("Jake's steps on Wednesday:", steps[2, 2])
+
+my_steps = steps[0]
+print("My steps...", my_steps)
+
+print("Updating my steps on Thursday to 5500.")
+
+steps[0, 3] = 5500
+print("My updated steps:", steps[0])
